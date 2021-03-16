9166e6e553eca3ec41f5d5bebb3afa9a27c4218a
tlangan <tanner_langan@sfu.ca>
2021-03-15 18:12:32 -0700

    Revert "Merge remote-tracking branch 'origin/version-one' into version-one"
    This reverts commit 0344fe33695fbb25a18185940ecdd0ced1168873

--- app/src/main/java/com/example/myfirstapp/NewAccountActivity.java
+++ app/src/main/java/com/example/myfirstapp/NewAccountActivity.java
@@ -1,12 +1,10 @@
 package com.example.myfirstapp;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import io.realm.Realm;
 
 public class NewAccountActivity extends AppCompatActivity {
@@ -19,22 +17,14 @@ public class NewAccountActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_new_account);
     Button mButton = findViewById(R.id.registerButton);
-    final TextView errorFieldOne = findViewById(R.id.registerSuccessOne);
-    final TextView errorFieldTwo = findViewById(R.id.registerSuccessTwo);
     mButton.setOnClickListener(new View.OnClickListener() {
       public void onClick(View v) {
         EditText nameEdit = findViewById(R.id.registerName);
         EditText passwordEdit = findViewById(R.id.registerPassword);
         String name = nameEdit.getText().toString();
         String password = passwordEdit.getText().toString();
-        temp = new Users(name, "");
-        while(!temp.updatePassword(password)) {
-            errorFieldOne.setVisibility(View.VISIBLE);
-            errorFieldTwo.setVisibility(View.VISIBLE);
-        }
-        errorFieldTwo.setVisibility(View.GONE);
-        errorFieldTwo.setTextColor(Color.GREEN);
-        errorFieldTwo.setText(getString(R.string.new_account_success));
+        temp = new Users(name, password);
+
         //open a realm and save temp to it
         try {
           realm = Realm.getDefaultInstance();
--- app/src/main/java/com/example/myfirstapp/WelcomeActivity.java
+++ app/src/main/java/com/example/myfirstapp/WelcomeActivity.java
@@ -1,9 +1,6 @@
 package com.example.myfirstapp;
 
 import android.content.Intent;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -48,31 +45,6 @@ public class WelcomeActivity extends AppCompatActivity {
         realm.close();
       }
     }
-
-    Button btn_log_in, btn_reg;
-    //Set the buttons
-    btn_log_in = findViewById(R.id.btn_login);
-    btn_reg = findViewById(R.id.btn_register);
-
-    //Set the event for the login button
-    btn_log_in.setOnClickListener(new View.OnClickListener(){
-      @Override
-      public void onClick(View v){
-        Intent intent = new Intent();
-        intent.setClass(WelcomeActivity.this,LoginActivity.class);
-        startActivity(intent);
-      }
-    });
-
-    //Set the event for register button
-    btn_reg.setOnClickListener(new View.OnClickListener(){
-      @Override
-      public void onClick(View v){
-        Intent intent = new Intent();
-        intent.setClass(WelcomeActivity.this,NewAccountActivity.class);
-        startActivity(intent);
-      }
-    });
 
   }
 
