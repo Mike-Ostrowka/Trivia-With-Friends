cf8b2186c9e4131419fc58ac41fe8ac8c3aaef23
Mike Ostrowka <mostrowk@sfu.ca>
2021-03-15 20:32:26 -0800

    Added greeting to WelcomeActivity

--- app/src/main/java/com/example/team8project/WelcomeActivity.java
+++ app/src/main/java/com/example/team8project/WelcomeActivity.java
@@ -3,6 +3,7 @@ package com.example.team8project;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 
@@ -12,6 +13,8 @@ public class WelcomeActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_welcome);
+    TextView greeting = findViewById(R.id.textViewGreeting);
+    greeting.setText(getString(R.string.greeting));
   }
 
   @Override
