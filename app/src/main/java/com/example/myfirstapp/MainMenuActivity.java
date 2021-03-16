823f10ce848ba8fd28ac0da9b53da9f6d7cde4d7
jiawei Yu <jiaweiy@sfu.ca>
2021-03-15 17:10:51 -0700

    Transfer the change in MainMenuActivity to WelcomeActivity

--- app/src/main/java/com/example/myfirstapp/MainMenuActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainMenuActivity.java
@@ -1,11 +1,7 @@
 package com.example.myfirstapp;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -13,29 +9,5 @@ public class MainMenuActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main_menu);
-    Button btn_log_in, btn_reg;
-    //Set the buttons
-    btn_log_in = findViewById(R.id.btn_login);
-    btn_reg = findViewById(R.id.btn_register);
-
-    //Set the event for the login button
-    btn_log_in.setOnClickListener(new View.OnClickListener(){
-      @Override
-      public void onClick(View v){
-        Intent intent = new Intent();
-        intent.setClass(MainMenuActivity.this,LoginActivity.class);
-        startActivity(intent);
-      }
-    });
-
-    //Set the event for register button
-    btn_reg.setOnClickListener(new View.OnClickListener(){
-      @Override
-      public void onClick(View v){
-        Intent intent = new Intent();
-        intent.setClass(MainMenuActivity.this,NewAccountActivity.class);
-        startActivity(intent);
-      }
-    });
   }
 }
