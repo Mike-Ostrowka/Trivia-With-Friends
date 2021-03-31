a2a31597e8b820c08c5dc6dcdfd0470fc436e94c
tlangan <tanner_langan@sfu.ca>
2021-03-30 18:25:27 -0700

    updated comments for logic behind loadQuestion

--- app/src/main/java/com/example/team8project/Game.java
+++ app/src/main/java/com/example/team8project/Game.java
@@ -4,6 +4,7 @@ package com.example.team8project;
 public class Game {
 
     public String playerOneSelection, playerTwoSelection, firstAnswer, secondAnswer, thirdAnswer, fourthAnswer, fifthAnswer;
+    public int questionCount = 0;
     public int startingTimerSecondsAnswer = 10;
     public int startingTimerSecondsRead = 10;
     public int playerOneScore, playerTwoScore = 0;
@@ -14,7 +15,22 @@ public class Game {
 
     public String loadQuestion() {
 
+        /*
+        Pseudo-Code for loading questions
+        -->load first question
+        -->load answers corresponding to question and randomize their order??(how to do this)
+        -->timer starts counting down from 10
+        -->when timer reaches 0, player has 10 seconds to answer
+        -->take player input and use checkPlayerAnswer() to compare to curent questions correct answer
+        -->if correct player gains 5 points
+        -->refresh GameActivity with next question(increment questionCount 1)-->most likely to be done using new activity(must research this)
+        -->game runs until player has 25 points(this is more in relation to multiplayer.
+        -->return to welcomeActivity
 
+
+
+
+         */
         questionList.separateQuestions();
         getCurrentQuestion[1] = questionList.correctAnswers[1];
         return getCurrentQuestion[1];
