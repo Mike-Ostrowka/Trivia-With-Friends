9dc55632e2704de6582e55c298d8494a4dbcdcb7
jiawei Yu <jiaweiy@sfu.ca>
2021-03-31 13:16:48 -0700

    upload some music and add the click sound to loggin and register buttom

--- app/src/main/java/com/example/team8project/MainMenuActivity.java
+++ app/src/main/java/com/example/team8project/MainMenuActivity.java
@@ -1,14 +1,26 @@
 package com.example.team8project;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import io.realm.Realm;
 
+@RequiresApi(api = VERSION_CODES.LOLLIPOP)
 public class MainMenuActivity extends AppCompatActivity {
 
   private loginPreferences session;
+  private SoundPool soundPool;
+  private int C_sound;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +28,34 @@ public class MainMenuActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main_menu);
     addAdmin();
     checkLogin();
+
+    if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+        AudioAttributes audioAttributes = new AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+            .build();
+
+        soundPool = new SoundPool.Builder()
+            .setMaxStreams(1)
+            .setAudioAttributes(audioAttributes)
+            .build();
+    } else {
+        soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+    }
+    C_sound = soundPool.load(this, R.raw.click, 1);
+
+  }
+
+  @SuppressLint("NonConstantResourceId")
+  public void playSound(View v) {
+    switch (v.getId()) {
+      case R.id.btn_login:
+        soundPool.play(C_sound, 1, 1, 0, 0, 1);
+        break;
+      case R.id.btn_register:
+        soundPool.play(C_sound, 1, 1, 0, 0, 1);
+        break;
+    }
   }
 
 
