3af2cd6ac7aeb0d9c7cf30fe7463cd197016e0b4
Mike Ostrowka <mostrowk@sfu.ca>
2021-03-30 23:27:12 -0800

    Added settings fragment

--- app/src/main/java/com/example/team8project/FaqActivity.java
+++ app/src/main/java/com/example/team8project/FaqActivity.java
@@ -1,7 +1,9 @@
 package com.example.team8project;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.widget.TextView;
 
@@ -11,6 +13,11 @@ public class FaqActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_faq);
+    //load toolbar
+    Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+    setSupportActionBar(myToolbar);
+    ActionBar ab = getSupportActionBar();
+    ab.setDisplayHomeAsUpEnabled(true);
     TextView mtextView = findViewById(R.id.tv_faq);
     mtextView.setText(Html.fromHtml(getString(R.string.faq_text)));
   }
