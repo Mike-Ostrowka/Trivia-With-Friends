2b7b8103899cbe58c0121ed7e633f7d55b7c3d0a
jiawei Yu <jiaweiy@sfu.ca>
2021-04-05 18:41:35 -0700

    add background music for FaqActivity and SettingsActivity

--- app/src/main/java/com/example/team8project/FaqActivity.java
+++ app/src/main/java/com/example/team8project/FaqActivity.java
@@ -1,13 +1,19 @@
 package com.example.team8project;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.widget.TextView;
+import com.huhx0015.hxaudio.audio.HXMusic;
+
 
 public class FaqActivity extends AppCompatActivity {
+
+  private int song;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,21 @@ public class FaqActivity extends AppCompatActivity {
     ab.setDisplayHomeAsUpEnabled(true);
     TextView mtextView = findViewById(R.id.tv_faq);
     mtextView.setText(Html.fromHtml(getString(R.string.faq_text)));
+
+    //load preferences
+    PreferenceManager.setDefaultValues(getApplicationContext(), R.xml.preferences, false);
+    SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+    boolean soundSwitch = sharedPref.getBoolean(SettingsActivity.KEY_PREF_SOUND_SWITCH, false);
+    //if switch value is false, disable music
+    if (soundSwitch) {
+      playMusic();
+    }
   }
+
+    private void playMusic() {
+      song = R.raw.smooth_jazz;
+      HXMusic.music().load(song).gapless(true).looped(true).play(this);
+    }
+
 }
+
--- app/src/main/java/com/example/team8project/SettingsActivity.java
+++ app/src/main/java/com/example/team8project/SettingsActivity.java
@@ -1,16 +1,21 @@
 package com.example.team8project;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import com.huhx0015.hxaudio.audio.HXMusic;
 
 public class SettingsActivity extends AppCompatActivity {
 
   public static final String KEY_PREF_SOUND_SWITCH = "sound_enabled";
+
+  private int song;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +24,21 @@ public class SettingsActivity extends AppCompatActivity {
     getSupportFragmentManager().beginTransaction()
         .replace(android.R.id.content, new SettingsFragment())
         .commit();
+
+    //load preferences
+    PreferenceManager.setDefaultValues(getApplicationContext(), R.xml.preferences, false);
+    SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+    boolean soundSwitch = sharedPref.getBoolean(SettingsActivity.KEY_PREF_SOUND_SWITCH, false);
+    //if switch value is false, disable music
+    if (soundSwitch) {
+      playMusic();
+    }
   }
 
+  private void playMusic() {
+    song = R.raw.smooth_jazz;
+    HXMusic.music().load(song).gapless(true).looped(true).play(this);
+  }
   public void changePass(View view) {
     Intent intent = new Intent();
     intent.setClass(SettingsActivity.this, ChangePasswordActivity.class);
