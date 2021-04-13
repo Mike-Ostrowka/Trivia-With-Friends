d7d6ed1ab037f7313f87a1628215ce42ac2b344e
Mike Ostrowka <mostrowk@sfu.ca>
2021-04-12 17:36:46 -0800

    Friends list now dynamic

--- app/src/main/java/com/example/team8project/FriendsListFragment.java
+++ app/src/main/java/com/example/team8project/FriendsListFragment.java
@@ -2,12 +2,16 @@ package com.example.team8project;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
+import io.realm.OrderedCollectionChangeSet;
 import io.realm.Realm;
+import io.realm.RealmChangeListener;
+import io.realm.RealmResults;
 import java.util.ArrayList;
 
 
@@ -30,6 +34,15 @@ public class FriendsListFragment extends Fragment {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    realm = Realm.getDefaultInstance();
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    if(realm == null) {
+      realm = Realm.getDefaultInstance();
+    }
   }
 
   @Override
@@ -40,9 +53,8 @@ public class FriendsListFragment extends Fragment {
     //open a realm and find logged in user
     session = new loginPreferences(getActivity().getApplicationContext());
     username = session.getusername();
-    realm = Realm.getDefaultInstance();
     current = realm.where(Users.class).equalTo("_id", username).findFirst();
-    realm.close();
+    addChangeListenerToRealm(realm);
     mView = myView;
     return myView;
   }
@@ -57,15 +69,11 @@ public class FriendsListFragment extends Fragment {
     }
     adapter = new TableAdapter(dataModels, getActivity().getApplicationContext(), getActivity(), current);
     listView.setAdapter(adapter);
-    Button refresh = mView.findViewById(R.id.btn_refresh);
-    refresh.setOnClickListener(v -> updateTable());
   }
 
   public void updateTable() {
     adapter.clear();
-    realm = Realm.getDefaultInstance();
     current = realm.where(Users.class).equalTo("_id", username).findFirst();
-    realm.close();
     ArrayList<Users> temp = new ArrayList<>();
     for (int i = 0; i < current.getSizeFriends(); i++) {
       temp.add(current.getFriend(i));
@@ -73,4 +81,28 @@ public class FriendsListFragment extends Fragment {
     adapter.addAll(temp);
     adapter.notifyDataSetChanged();
   }
+
+    //change listener on local realm
+  private void addChangeListenerToRealm(Realm realm) {
+    // all tasks in the realm
+    RealmResults<Users> tasks = realm.where(Users.class).equalTo("_id", current.getUserName()).findAll();
+    tasks.addChangeListener(users -> {
+      updateTable();
+      tasks.removeAllChangeListeners();
+      updateListener();
+    });
+  }
+
+  private void updateListener() {
+    addChangeListenerToRealm(realm);
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    if(realm != null) {
+      realm.close();
+    }
+  }
+
 }
